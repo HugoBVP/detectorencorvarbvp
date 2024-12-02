214e0f8aaf81261efb1780331943c99f6afda6f9
HugoBermúdez <hugo.bermdz@gmail.com>
2024-12-01 20:47:29 -0600

    Actualización de app.py y index.html para usar MediaPipe correctamente en el despliegue

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
+# Ruta para la página principal que sirve el index.html
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# Ejecutar la aplicación en el host 0.0.0.0 y puerto 5000
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
