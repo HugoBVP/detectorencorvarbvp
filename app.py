a1c7fa1520c588f3e498b57008b5d2e606cffa98
HugoBermúdez <hugo.bermdz@gmail.com>
2024-12-01 21:06:36 -0600

    Descripción de los cambios

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from flask import Flask, render_template
-import os
 
 app = Flask(__name__)
 
-# Ruta para la página principal que sirve el index.html
+# Ruta para servir el archivo index.html
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Ejecutar la aplicación en el host 0.0.0.0 y puerto 5000
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True, host="127.0.0.1", port=5000)
+
